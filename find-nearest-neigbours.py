5d4d1a09cf301b830067ad5c387b788282142708
Prince Adeyemi <prince@vegasnewspaper.com>
2017-11-20 15:13:36 -0800

    Update find-nearest-neigbours.py

--- find-nearest-neigbours.py
+++ find-nearest-neigbours.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def find_nearest_neighbors(p, points, k=5):
     """ 
-    Find the k nearest neighbors of points and return the indexes of closest point(s)
+    Find the k nearest neighbors of points and return the indexes of nearest neigbor k
     usage: 
         points = np.array([[1,1],[1,2],[1,3],[2,1],[2,2],[2,3],[3,1],[3,2],[3,3]])
         p = np.array(2.5, 2)
