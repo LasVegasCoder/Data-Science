a9ac88f9021c874eb34e7a42893d1f5ddf012119
Prince Adeyemi <prince@vegasnewspaper.com>
2018-01-20 15:43:28 -0800

    Update find-nearest-neigbours.py
        Find the k nearest neighbors of points and return the indexes of nearest neigbor k
        usage: 
            //Generate points.
            points = np.array([[1,1],[1,2],[1,3],[2,1],[2,2],[2,3],[3,1],[3,2],[3,3]])
            p = np.array(2.5, 2)
            
            //Get the index of nearest points
            ind = find_nearest_neighbors(p, points, 2); 
        
            //Print result.
            print(points[ind]);
            
            OUTPUT:  
                [[2 2]
                [3 2]]

--- find-nearest-neigbours.py
+++ find-nearest-neigbours.py
@@ -4,10 +4,15 @@ def find_nearest_neighbors(p, points, k=5):
     """ 
     Find the k nearest neighbors of points and return the indexes of nearest neigbor k
     usage: 
+        //Generate points.
         points = np.array([[1,1],[1,2],[1,3],[2,1],[2,2],[2,3],[3,1],[3,2],[3,3]])
         p = np.array(2.5, 2)
         
-        ind = find_nearest_neighbors(p, points, 2); print(points[ind])
+        //Get the index of nearest points
+        ind = find_nearest_neighbors(p, points, 2); 
+    
+        //Print result.
+        print(points[ind]);
         
         OUTPUT:  
             [[2 2]
