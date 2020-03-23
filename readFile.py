3317dfb88c3be7424abfe8e8d103bc4f0e898877
Prince Adeyemi <23622619+LasVegasCoder@users.noreply.github.com>
2020-03-22 17:30:17 -0700

    Update readFile.py
    Index fixed

--- readFile.py
+++ readFile.py
@@ -1,7 +1,6 @@
+""" Read file into strings, removed new line and carriage return then return the strings"""
 def readFile(file_path):
-   """ Read file into strings, removed new line and carriage return then return the strings"""
     with open(file_path, 'r', 'utf-8') as current_file:
         texts = current_file.read()
         text  = texts.current_file.replace("\n","").replace("\r","")
-        
     return text
